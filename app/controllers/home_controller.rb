4fc1392d4294f056f052fcb909e8ffdff2e5fc29
Ian Bayne <ianbayne@gmail.com>
2019-04-28 16:08:59 +0900

    change: include own posts in index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,13 @@ class HomeController < ApplicationController
 
   def index
     posts_ids = []
+    own_posts_ids = current_user.posts.pluck(:id)
+    posts_ids << own_posts_ids
+
     current_user.friends.each do |friend|
       posts_ids << friend.posts.pluck(:id)
     end
+
     posts_ids = posts_ids.flatten
 
     @posts = Post.where(id: posts_ids).order(created_at: :desc)
