6fce56066497f06f6529154379da9861bca84a11
Ian Bayne <ianbayne@gmail.com>
2019-04-28 14:48:27 +0900

    test: creation and destruction of friendships

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @ten_k  = users(:ten_k)
+    @murphy = users(:murphy)
+    @ten_k.friendships.create!(friend: @murphy)
+  end
+
+  test 'creating a friendship creates the inverse friendship as well' do
+    assert_equal(1, @ten_k.friends.count)
+    assert_equal(1, @murphy.friends.count)
+    assert_equal(2, Friendship.count)
+  end
+
+  test 'destroying a friendship destroys the inverse friendship as well' do
+    Friendship.find_by(user: @murphy, friend: @ten_k).destroy
+
+    assert_equal(0, Friendship.count)
+  end
 end
