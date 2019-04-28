5407de121599189580bf4ea5c917c34814da3e1f
Ian Bayne <ianbayne@gmail.com>
2019-04-28 16:05:47 +0900

    change: separate shrine image storage locations for dev and test

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,5 +1,4 @@
 require 'shrine'
-require 'shrine/storage/s3'
 
 s3_options = {
   region:            ENV['aws_region'],
@@ -8,10 +7,28 @@ s3_options = {
   secret_access_key: ENV['aws_secret_access_key']
 }
 
-Shrine.storages = {
-  cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
-  store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)
-}
+if Rails.env.development?
+  require 'shrine/storage/file_system'
+
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new('tmp', prefix: 'cache'),
+    store: Shrine::Storage::FileSystem.new('tmp', prefix: 'store')
+  }
+elsif Rails.env.test?
+  require 'shrine/storage/memory'
+
+  Shrine.storages = {
+    cache: Shrine::Storage::Memory.new,
+    store: Shrine::Storage::Memory.new
+  }
+else
+  require 'shrine/storage/s3'
+
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+    store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)
+  }
+end
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
